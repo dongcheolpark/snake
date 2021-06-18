da6cd01527e1a49235dee8fbf140434173cb3107
dongcheolpark <head02325@soongsil.ac.kr>
2021-06-18 16:43:56 +0900

    a

---
+++ graphic/window.h
@@ -0,0 +1,15 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+#include "../info/info.h"
+
+class window {
+private :
+	Info * UserData;
+public:
+	window(Info * a); 
+	void draw();
+	void title();
+	void clear();
+};
+
+#endif
